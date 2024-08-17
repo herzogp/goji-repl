43e95efca0b7ded3a892060576d7dbddfc58982b
Paul Herzog <pmherzog@gmail.com>
2024-08-17 11:52:13 -0400

    Renamed NodeItem to Node

---
+++ node.py
@@ -0,0 +1,54 @@
+from enum import Enum
+
+class NodeType(Enum):
+    ATOM = 1
+    LIST = 2
+
+# A Node can be either a single ATOM, 
+# or a LIST (of other Nodes)
+# In either case, this is called an Expression
+class Node:
+    def __init__(self, node_typ):
+        self._typ = node_typ
+        self.exprs = []
+   
+    # expr is either an Atom or a List
+    def add(self, expr):
+        self.exprs.append(expr)
+
+    def get_value(self):
+        if not self.isatom():
+            return None
+        return self.exprs[0]
+
+    def get_item(self, idx):
+        if idx < 0 or idx >= self.__len__():
+            return None
+        else:
+            return self.exprs[idx]
+
+    def isatom(self):
+        return self._typ == NodeType.ATOM
+
+    def get_list(self):
+        if not self.islist():
+            return None
+        return self.exprs[0]
+
+    def islist(self):
+        return self._typ == NodeType.LIST
+
+    def did_apply_symbol(self, s):
+        if s != '#':
+            return False
+        atom_val = self.get_value()
+        if atom_val == None:
+            return False
+        return self.exprs[0].did_apply_symbol(s)
+
+    def __len__(self):
+        return len(self.exprs)
+
+    def __str__(self):
+        args = ', '.join([str(item) for item in self.exprs])
+        return '#%s(%s)' % (self._typ.name, args)
